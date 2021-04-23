c75a5775f4edd7eca8de9a5d013c02c967b78be3
Kristjan <kristjaneimre@gmail.com>
2021-04-23 16:59:39 +0200

    pymol parser more robust and slight style change

--- aiida_nanotech_empa/parsers/cubegen_pymol_parser.py
+++ aiida_nanotech_empa/parsers/cubegen_pymol_parser.py
@@ -10,12 +10,14 @@ import aiida_nanotech_empa.utils.pymol_render as pr
 
 from aiida.plugins import ParserFactory
 
+import aiida_gaussian.utils.cube
+
 CubegenBaseParser = ParserFactory('cubegen_base_parser')
 
 
 class CubegenPymolParser(CubegenBaseParser):
     """Cubegen parser based on pymol."""
-    def _parse_folders(self, retrieved_folder_paths, parser_params):
+    def _parse_folders(self, retrieved_folders, parser_params):
 
         if 'isovalues' in parser_params:
             isovalues = parser_params['isovalues']
@@ -24,17 +26,33 @@ class CubegenPymolParser(CubegenBaseParser):
 
         image_folder = tempfile.TemporaryDirectory()
 
-        for folder_path in retrieved_folder_paths:
-            for filename in os.listdir(folder_path):
-                filepath = os.path.join(folder_path, filename)
+        for retrieved_fd in retrieved_folders:
+            for filename in retrieved_fd.list_object_names():
+                if filename.endswith(".cube"):
 
-                if filepath.endswith(".cube"):
-                    for iv in isovalues:
-                        pr.make_pymol_png(filepath,
-                                          isov=iv,
-                                          colors=[(0.9, 0.15, 0.15),
-                                                  (0.0, 0.35, 0.9)],
-                                          output_folder=image_folder.name)
+                    # AiiDA retrieved folder provides handles to retrieved files
+                    # pymol rendering, however, requires the path to the cube file
+                    # therefore, we need to write the contents to a temporary file
+
+                    cube = aiida_gaussian.utils.cube.Cube()
+                    with retrieved_fd.open(filename) as handle:
+                        cube.read_cube(handle)
+
+                    with tempfile.NamedTemporaryFile(mode='w+',
+                                                     encoding='utf-8',
+                                                     suffix='.cube') as tf:
+                        cube.write_cube_file(tf.name)
+
+                        for iv in isovalues:
+                            pr.make_pymol_png(
+                                tf.name,
+                                isov=iv,
+                                colors=[
+                                    (1.0, 0.2, 0.2),  # red
+                                    (0.0, 0.4, 1.0),  # blue
+                                ],
+                                output_folder=image_folder.name,
+                                output_name=os.path.splitext(filename)[0])
 
         image_folder_node = FolderData(tree=image_folder.name)
 
