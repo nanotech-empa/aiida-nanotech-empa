6d527aceef164980033f5eb878777e1f4c282a8d
c.pignedoli <c.pignedoli@gmail.com>
2021-08-05 10:43:10 +0000

    example still not working

---
+++ examples/workflows/example_cp2k_slabopt.py
@@ -0,0 +1,58 @@
+from ase import Atoms
+from ase.io import read
+
+from aiida.orm import StructureData, Bool, Int, List
+from aiida.orm import load_code
+from aiida.engine import run_get_node
+from aiida.plugins import WorkflowFactory
+
+Cp2kSlabOptWorkChain = WorkflowFactory('nanotech_empa.cp2k.slab_opt')
+
+
+def _example_cp2k_slabopt(cp2k_code, mult):
+
+    builder = Cp2kSlabOptWorkChain.get_builder()
+
+    builder.metadata.label = 'Cp2kSlabOptWorkChain'
+    builder.metadata.description = 'test description'
+    builder.code = cp2k_code
+    builder.walltime_seconds = Int(600)
+    ase_geom = read('test_slab.xyz')
+    builder.structure = StructureData(ase=ase_geom)
+    builder.max_nodes = Int(1)
+    builder.fixed_atoms=Str('3..66')
+
+    builder.multiplicity = Int(mult)
+    mag = [0 for i in ase_geom]
+    if mult == 1:
+        mag[0] = 1
+        mag[1] = -1
+        builder.magnetization_per_site = List(list=mag)
+
+    builder.debug = Bool(True)
+
+    _, calc_node = run_get_node(builder)
+
+    assert calc_node.is_finished_ok
+
+    slabopt_out_dict = dict(calc_node.outputs.output_parameters)
+    print()
+    for k in slabopt_out_dict:
+        print("  {}: {}".format(k, slabopt_out_dict[k]))
+
+
+def example_cp2k_slabopt_rks(cp2k_code):
+    _example_cp2k_slabopt(cp2k_code, 0)
+
+
+def example_cp2k_slabopt_uks(cp2k_code):
+    _example_cp2k_slabopt(cp2k_code, 1)
+
+
+if __name__ == '__main__':
+    for mult in [0]:
+        print()
+        print("####################################")
+        print("####  mult={}".format(mult))
+        print("####################################")
+        _example_cp2k_slabopt(load_code("cp2k@localhost"), mult)
