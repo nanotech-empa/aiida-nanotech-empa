1fad57e18b33fcf435190ec13bd7dcc0768c2cc1
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2022-03-07 18:01:44 +0100

    Use setup.cfg instead of setup.json (#70)
    Also, remove `check_version.py` and place the version number
    into `version.py`.

---
+++ aiida_nanotech_empa/version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module contains project version information."""
+
+__version__ = "0.5.2"
--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-from setuptools import setup, find_packages
-import json
+"""This file is required for editable installs of the package."""
+from setuptools import setup
 
-if __name__ == '__main__':
-    # Provide static information in setup.json
-    # such that it can be discovered automatically
-    with open('setup.json', 'r', encoding='utf-8') as info:
-        kwargs = json.load(info)
-    with open('README.md', encoding='utf-8') as readmef:
-        readme = readmef.read()
-    setup(
-        packages=find_packages(),
-        # this doesn't work when placed in setup.json (something to do with str type)
-        package_data={
-            "": ["*"],
-        },
-        long_description=readme,
-        long_description_content_type='text/markdown',
-        **kwargs)
+setup()
