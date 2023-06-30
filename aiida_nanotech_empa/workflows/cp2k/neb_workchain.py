8e7cf3f057078a223ddb0f9b007254b459169758
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2023-06-30 15:38:52 +0000

    added rotate_frames

--- aiida_nanotech_empa/workflows/cp2k/neb_workchain.py
+++ aiida_nanotech_empa/workflows/cp2k/neb_workchain.py
@@ -144,7 +144,7 @@ class Cp2kNebWorkChain(engine.WorkChain):
         # NEB parameters.
         for param in [
             "align_frames",
-            "band_type",
+            "rotate_frames" "band_type",
             "k_spring",
             "nproc_rep",
             "number_of_replica",
