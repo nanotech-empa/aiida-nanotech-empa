9bf7e53c9a653c22024f5f26e59404b3b3b2c3bb
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2023-08-11 17:06:35 +0200

    Unify UKS settings across different cp2k work chains (#136)

--- aiida_nanotech_empa/workflows/cp2k/fragment_separation.py
+++ aiida_nanotech_empa/workflows/cp2k/fragment_separation.py
@@ -1,67 +1,14 @@
 import pathlib
 
-import numpy as np
 from aiida import engine, orm, plugins
 
-from ...utils import analyze_structure, common_utils
+from ...utils import analyze_structure, common_utils, split_structure
 from . import cp2k_utils
 
 StructureData = plugins.DataFactory("core.structure")
 Cp2kBaseWorkChain = plugins.WorkflowFactory("cp2k.base")
 
 DATA_DIR = pathlib.Path(__file__).parent.absolute() / "data"
-
-
-# @engine.calcfunction
-def split_structure(structure, fixed_atoms, magnetization_per_site, fragments):
-    ase_geo = structure.get_ase()
-
-    allfixed = [0 for i in ase_geo]
-    mps = []
-    fixed = ""
-    if "all" not in fragments:
-        yield {
-            "label": "all",
-            "structure": structure,
-            "fixed_atoms": fixed_atoms,
-            "magnetization_per_site": magnetization_per_site,
-        }
-
-    for f in fixed_atoms:
-        allfixed[f] = 1
-
-    for fragment_label, fragment in fragments.items():
-        fragment = sorted(fragment)
-
-        if magnetization_per_site or fixed_atoms:
-            tuples = [
-                (e, *np.round(p, 2))
-                for e, p in zip(
-                    ase_geo[fragment].get_chemical_symbols(),
-                    ase_geo[fragment].positions,
-                )
-            ]
-            if magnetization_per_site:
-                mps = [
-                    m
-                    for at, m in zip(ase_geo, list(magnetization_per_site))
-                    if (at.symbol, *np.round(at.position, 2)) in tuples
-                ]
-                if all(m == 0 for m in mps):
-                    mps = []
-            if fixed_atoms:
-                fixed = [
-                    f
-                    for at, f in zip(ase_geo, allfixed)
-                    if (at.symbol, *np.round(at.position, 2)) in tuples
-                ]
-
-        yield {
-            "label": fragment_label,
-            "structure": StructureData(ase=ase_geo[fragment]),
-            "fixed_atoms": orm.List(list=np.nonzero(fixed)[0].tolist()),
-            "magnetization_per_site": orm.List(list=mps),
-        }
 
 
 class Cp2kFragmentSeparationWorkChain(engine.WorkChain):
@@ -172,7 +119,7 @@ class Cp2kFragmentSeparationWorkChain(engine.WorkChain):
     def run_scfs(self):
         """Run SCF calculation for the whole system and for fragments."""
         # Firt run SCF for the whole system and its fragments.
-        for inputs in split_structure(
+        for inputs in split_structure.split_structure(
             structure=self.inputs.structure,
             fixed_atoms=self.inputs.fixed_atoms,
             magnetization_per_site=self.inputs.dft_params["magnetization_per_site"]
