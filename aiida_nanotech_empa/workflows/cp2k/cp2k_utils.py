6596cdeca84227a36afa15160e28bf5f8b95ae74
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2023-11-23 17:01:03 +0000

    bug create_a_copy wfn

--- aiida_nanotech_empa/workflows/cp2k/cp2k_utils.py
+++ aiida_nanotech_empa/workflows/cp2k/cp2k_utils.py
@@ -417,6 +417,7 @@ def structure_available_wfn(
         # In all other cases, e.g. geo opt, replica, ...
         # use the standard name
         wfn_name = "aiida-RESTART.wfn"
+        create_a_copy = False
 
     wfn_exists = False
     try:
