af2d6ec32de02bcc2a1a229d5cbf8e8f93ec0a77
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2026-05-27 08:58:14 +0000

    Add Bader analysis to SCF workchain

--- aiida_nanotech_empa/workflows/cp2k/scf_workchain.py
+++ aiida_nanotech_empa/workflows/cp2k/scf_workchain.py
@@ -3,6 +3,7 @@ from aiida import engine, orm, plugins
 from ...utils import common_utils
 from .diag_workchain import Cp2kDiagWorkChain
 
+BaderCalculation = plugins.CalculationFactory("nanotech_empa.bader")
 SparseOverlapCalculation = plugins.CalculationFactory("nanotech_empa.sparse_overlap")
 
 
@@ -48,6 +49,26 @@ class Cp2kScfWorkChain(Cp2kDiagWorkChain):
             required=False,
             help="Absolute-value threshold for retrieved sparse overlap entries.",
         )
+        spec.input(
+            "compute_bader_charges",
+            valid_type=orm.Bool,
+            default=lambda: orm.Bool(False),
+            required=False,
+            help="Run Bader charge analysis on the OT charge-density cube.",
+        )
+        spec.input(
+            "bader_code",
+            valid_type=orm.Code,
+            required=False,
+            help="Bader executable configured for the nanotech_empa.bader plugin.",
+        )
+        spec.input(
+            "bader_cutoff",
+            valid_type=orm.Float,
+            default=lambda: orm.Float(1200.0),
+            required=False,
+            help="Plane-wave cutoff used for the OT charge-density cube for Bader analysis.",
+        )
         spec.outline(
             cls.setup,
             cls.run_ot_scf,
@@ -56,6 +77,9 @@ class Cp2kScfWorkChain(Cp2kDiagWorkChain):
                 engine.if_(cls.should_run_sparse_overlap)(
                     cls.run_sparse_overlap,
                 ),
+            ),
+            engine.if_(cls.should_run_bader)(
+                cls.run_bader,
             ),
             cls.finalize,
         )
@@ -71,8 +95,18 @@ class Cp2kScfWorkChain(Cp2kDiagWorkChain):
                 "'sparse_overlap_code' is required when "
                 "'retrieve_sparse_overlap' is True."
             )
+        compute_bader_charges = value.get("compute_bader_charges")
+        if (
+            compute_bader_charges is not None
+            and compute_bader_charges.value
+            and "bader_code" not in value
+        ):
+            return "'bader_code' is required when 'compute_bader_charges' is True."
 
     def should_run_diag_scf(self):
+        if self.should_run_bader():
+            return False
+
         dft_params = self.inputs.dft_params.get_dict()
         return (
             self.inputs.write_overlap_matrix.value
@@ -80,8 +114,24 @@ class Cp2kScfWorkChain(Cp2kDiagWorkChain):
             or dft_params.get("added_mos", 0) > 0
         )
 
+    def should_run_bader(self):
+        return self.inputs.compute_bader_charges.value
+
     def should_run_sparse_overlap(self):
         return self.inputs.retrieve_sparse_overlap.value
+
+    def update_ot_input_dict(self, input_dict):
+        if not self.should_run_bader():
+            return
+
+        input_dict["FORCE_EVAL"]["DFT"]["MGRID"]["CUTOFF"] = (
+            self.inputs.bader_cutoff.value
+        )
+        print_section = input_dict["FORCE_EVAL"]["DFT"].setdefault("PRINT", {})
+        charge_density = print_section.setdefault("E_DENSITY_CUBE", {})
+        charge_density["STRIDE"] = "1 1 1"
+        charge_density.setdefault("EACH", {"QS_SCF": "0", "GEO_OPT": "0"})
+        charge_density.setdefault("ADD_LAST", "NUMERIC")
 
     def update_diag_input_dict(self, input_dict):
         if not (
@@ -122,7 +172,49 @@ class Cp2kScfWorkChain(Cp2kDiagWorkChain):
         }
         return engine.ToContext(sparse_overlap=self.submit(builder))
 
+    def run_bader(self):
+        if not common_utils.check_if_calc_ok(self, self.ctx.ot_scf):
+            self.report("OT SCF failed")
+            return self.exit_codes.ERROR_TERMINATION
+
+        self.report("Running Bader charge analysis")
+        builder = BaderCalculation.get_builder()
+        builder.code = self.inputs.bader_code
+        builder.parent_calc_folder = self.ctx.ot_scf.outputs.remote_folder
+        builder.charge_density_filename = orm.Str("aiida-ELECTRON_DENSITY-1_0.cube")
+        builder.metadata = {
+            "label": "bader",
+            "options": {
+                "resources": {
+                    "num_machines": 1,
+                    "num_mpiprocs_per_machine": 1,
+                    "num_cores_per_mpiproc": 1,
+                },
+                "max_wallclock_seconds": min(
+                    3600, self.ctx.options["max_wallclock_seconds"]
+                ),
+                "withmpi": False,
+            },
+        }
+        return engine.ToContext(bader=self.submit(builder))
+
     def finalize(self):
+        if self.should_run_bader():
+            if not common_utils.check_if_calc_ok(self, self.ctx.ot_scf):
+                self.report("OT SCF failed")
+                return self.exit_codes.ERROR_TERMINATION
+
+            if not common_utils.check_if_calc_ok(self, self.ctx.bader):
+                self.report("Bader charge analysis failed")
+                return self.exit_codes.ERROR_TERMINATION
+
+            self.out("output_parameters", self.ctx.ot_scf.outputs.output_parameters)
+            self.out("remote_folder", self.ctx.ot_scf.outputs.remote_folder)
+            self.out("retrieved", self.ctx.ot_scf.outputs.retrieved)
+            self.out("bader_retrieved", self.ctx.bader.outputs.retrieved)
+            self.report("Work chain is finished")
+            return None
+
         run_diag = self.should_run_diag_scf()
         final_calc = self.ctx.diag_scf if run_diag else self.ctx.ot_scf
         calc_label = "diagonalization scf" if run_diag else "OT SCF"
