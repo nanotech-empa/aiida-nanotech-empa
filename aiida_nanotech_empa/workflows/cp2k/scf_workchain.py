206569ff8a0669a06b719e83abf7691b87d6005f
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2026-06-15 08:21:54 +0000

    Add Lowdin charge output for CP2K workflows

--- aiida_nanotech_empa/workflows/cp2k/scf_workchain.py
+++ aiida_nanotech_empa/workflows/cp2k/scf_workchain.py
@@ -1,6 +1,7 @@
 from aiida import engine, orm, plugins
 
 from ...utils import common_utils
+from . import cp2k_utils
 from .diag_workchain import Cp2kDiagWorkChain
 
 BaderCalculation = plugins.CalculationFactory("nanotech_empa.bader")
@@ -196,6 +197,7 @@ class Cp2kScfWorkChain(Cp2kDiagWorkChain):
         return self.inputs.compute_unfolding.value
 
     def update_ot_input_dict(self, input_dict):
+        cp2k_utils.apply_default_charge_analysis(input_dict)
         if not self.should_run_bader():
             return
 
