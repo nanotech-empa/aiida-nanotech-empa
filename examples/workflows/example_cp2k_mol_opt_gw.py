f079408f2c4d0f8f9d271ceaf8838e33848bbedb
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2026-07-21 07:34:31 +0000

    Remove unrelated formatting changes

--- examples/workflows/example_cp2k_mol_opt_gw.py
+++ examples/workflows/example_cp2k_mol_opt_gw.py
@@ -53,7 +53,8 @@ def _geo_opt_cp2k_builder(cp2k_code):
     )
 
     data_dir = (
-        pathlib.Path(__file__).parents[2] / "aiida_nanotech_empa/workflows/cp2k/data"
+        pathlib.Path(__file__).parents[2]
+        / "aiida_nanotech_empa/workflows/cp2k/data"
     )
 
     builder = Cp2kCalculation.get_builder()
